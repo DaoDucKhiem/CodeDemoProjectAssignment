6f0d4d952c53b237bf5906ec43f6aec4044fa493
DaoDucKhiem <daoduckhiem1104@gmail.com>
2021-05-12 22:21:40 +0700

    project DuAnDemo

---
+++ Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿
+namespace AssignmentInternship
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Button btnFileCom;
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnFile = new System.Windows.Forms.Button();
+            this.txtFileSV = new System.Windows.Forms.TextBox();
+            this.dataGrid = new System.Windows.Forms.DataGridView();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtFileCom = new System.Windows.Forms.TextBox();
+            this.btnExecute = new System.Windows.Forms.Button();
+            btnFileCom = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnFileCom
+            // 
+            btnFileCom.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            btnFileCom.Location = new System.Drawing.Point(1108, 64);
+            btnFileCom.Name = "btnFileCom";
+            btnFileCom.Size = new System.Drawing.Size(150, 46);
+            btnFileCom.TabIndex = 1;
+            btnFileCom.Text = "browse";
+            btnFileCom.UseCompatibleTextRendering = true;
+            btnFileCom.UseVisualStyleBackColor = true;
+            btnFileCom.Click += new System.EventHandler(this.btnFileCom_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(268, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(918, 59);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Phân công sinh viên thực tập tại các công ty";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.Click += new System.EventHandler(this.btnFile_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.btnFile);
+            this.groupBox1.Controls.Add(this.txtFileSV);
+            this.groupBox1.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.groupBox1.Location = new System.Drawing.Point(67, 131);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1293, 141);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Danh sách sinh viên";
+            // 
+            // btnFile
+            // 
+            this.btnFile.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnFile.Location = new System.Drawing.Point(1108, 63);
+            this.btnFile.Name = "btnFile";
+            this.btnFile.Size = new System.Drawing.Size(150, 46);
+            this.btnFile.TabIndex = 1;
+            this.btnFile.Text = "browse";
+            this.btnFile.UseVisualStyleBackColor = true;
+            this.btnFile.Click += new System.EventHandler(this.btnFile_Click);
+            // 
+            // txtFileSV
+            // 
+            this.txtFileSV.Location = new System.Drawing.Point(34, 66);
+            this.txtFileSV.Name = "txtFileSV";
+            this.txtFileSV.ReadOnly = true;
+            this.txtFileSV.Size = new System.Drawing.Size(1048, 43);
+            this.txtFileSV.TabIndex = 0;
+            // 
+            // dataGrid
+            // 
+            this.dataGrid.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid.Location = new System.Drawing.Point(67, 543);
+            this.dataGrid.Name = "dataGrid";
+            this.dataGrid.RowHeadersWidth = 82;
+            this.dataGrid.RowTemplate.Height = 41;
+            this.dataGrid.Size = new System.Drawing.Size(1293, 523);
+            this.dataGrid.TabIndex = 2;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(btnFileCom);
+            this.groupBox2.Controls.Add(this.txtFileCom);
+            this.groupBox2.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.groupBox2.Location = new System.Drawing.Point(67, 302);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(1293, 150);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Danh sách công ty";
+            // 
+            // txtFileCom
+            // 
+            this.txtFileCom.Location = new System.Drawing.Point(34, 64);
+            this.txtFileCom.Name = "txtFileCom";
+            this.txtFileCom.ReadOnly = true;
+            this.txtFileCom.Size = new System.Drawing.Size(1048, 43);
+            this.txtFileCom.TabIndex = 0;
+            // 
+            // btnExecute
+            // 
+            this.btnExecute.Location = new System.Drawing.Point(584, 474);
+            this.btnExecute.Name = "btnExecute";
+            this.btnExecute.Size = new System.Drawing.Size(150, 46);
+            this.btnExecute.TabIndex = 4;
+            this.btnExecute.Text = "Thực hiện";
+            this.btnExecute.UseVisualStyleBackColor = true;
+            this.btnExecute.Click += new System.EventHandler(this.btnExecute_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1425, 1091);
+            this.Controls.Add(this.btnExecute);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.dataGrid);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtFileSV;
+        private System.Windows.Forms.DataGridView dataGrid;
+        private System.Windows.Forms.Button btnFile;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnFileCom;
+        private System.Windows.Forms.TextBox txtFileCom;
+        private System.Windows.Forms.Button btnExecute;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,377 @@
+﻿using ClosedXML.Excel;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Windows.Forms;
+
+namespace AssignmentInternship
+{
+    public partial class Form1 : Form
+    {
+        // mảng chứa danh sách nguyện vọng của sinh viên
+        List<Student> listStudent = new List<Student>();
+
+        // danh sách công ty và số lượng tuyển dụng
+        List<Company> listCompany = new List<Company>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// khi btn chọn file danh sách sinh viên click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fdlg = new OpenFileDialog();
+            fdlg.Title = "Chọn file danh sách sinh viên";
+            fdlg.Filter = "excel file | *.xls; *.xlsx";
+            fdlg.FilterIndex = 1;
+            fdlg.Multiselect = false;
+            fdlg.RestoreDirectory = true;
+
+            // nếu chọn file thành công
+            if (fdlg.ShowDialog() == DialogResult.OK)
+            {
+                txtFileSV.Text = fdlg.FileName;
+                using (XLWorkbook workbook = new XLWorkbook(fdlg.FileName))
+                {
+                    bool isFirstRow = true;
+                    var rows = workbook.Worksheet(1).RowsUsed();
+                    foreach (var row in rows)
+                    {
+                        // header file excel: stt, mssv, họ và tên, GPA, nguyện vọng
+                        if (isFirstRow)
+                        {
+                            isFirstRow = false;
+                        }
+                        else
+                        {
+                            int i = 0;
+                            Student item = new Student();
+                            item.listWish = new string[0];
+                            foreach (IXLCell cell in row.Cells())
+                            {
+                                // cột mã số sinh viên
+                                if (i == 1)
+                                {
+                                    item.Code = cell.Value.ToString();
+                                }
+
+                                // cột họ và tên
+                                if (i == 2)
+                                {
+                                    item.Name = cell.Value.ToString();
+                                }
+
+                                // cột GPA
+                                if (i == 3)
+                                {
+                                    item.GPA = (double)cell.Value;
+                                }
+
+                                // nếu là cột nguyện vọng
+                                if (i == 4)
+                                {
+                                    string data = cell.Value.ToString();
+                                    // mảng danh sách công ty mong muốn thực tập theo thứ tự ưu tiên từ đầu đến cuối
+                                    var listWish = data.Split(",");
+                                    item.listWish = listWish;
+                                }
+
+                                i++;
+                            }
+
+                            listStudent.Add(item);
+                        }
+                    }
+
+                    //dataGrid.DataSource = dt.DefaultView;
+                    //Cursor.Current = Cursors.Default;
+                }
+            }
+        }
+
+        /// <summary>
+        /// khi btn chọn danh sách công ty click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnFileCom_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fdlg = new OpenFileDialog();
+            fdlg.Title = "Chọn file danh sách công ty";
+            fdlg.Filter = "excel file | *.xls; *.xlsx";
+            fdlg.FilterIndex = 1;
+            fdlg.Multiselect = false;
+            fdlg.RestoreDirectory = true;
+
+            // nếu chọn file thành công
+            if (fdlg.ShowDialog() == DialogResult.OK)
+            {
+                txtFileCom.Text = fdlg.FileName;
+                using (XLWorkbook workbook = new XLWorkbook(fdlg.FileName))
+                {
+                    bool isFirstRow = true;
+                    var rows = workbook.Worksheet(1).RowsUsed();
+                    foreach (var row in rows)
+                    {
+                        // header của file: stt, mã công ty, tên công ty, số lượng tuyển dụng
+                        if (isFirstRow)
+                        {
+                            isFirstRow = false;
+                        }
+                        else
+                        {
+                            int i = 0;
+                            Company com = new Company();
+                            foreach (IXLCell cell in row.Cells())
+                            {
+                                // cột mã công ty
+                                if (i == 1)
+                                {
+                                    com.Code = cell.Value.ToString();
+                                }
+
+                                // cột tên công ty
+                                if (i == 2)
+                                {
+                                    com.Name = cell.Value.ToString();
+                                }
+
+                                // cột số lượng tuyển dụng
+                                if (i == 3)
+                                {
+                                    com.Quantity = (double)cell.Value;
+                                }
+
+                                i++;
+                            }
+
+                            // thêm công ty vào danh sách
+                            listCompany.Add(com);
+                        }
+                    }
+                }
+            }
+        }
+        
+        /// <summary>
+        /// thực hiện ghép nối khi nhấn click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnExecute_Click(object sender, EventArgs e)
+        {
+            // số lượng sinh viên
+            int cSt = listStudent.Count;
+
+            // số lượng công ty
+            int cCo = listCompany.Count;
+
+            // danh sách sinh viên đã match theo công ty
+            // mục đích là để so sánh khi có nhiều hơn số lượng sinh viên
+            // nộp vào công ty so với số lượng tuyển dụng của công ty đó
+            Dictionary<string, List<Student>> listMatched = new Dictionary<string, List<Student>>();
+
+            // list lưu lại công ty mà sinh vien đã nộp đơn.
+            Dictionary<string, bool[]> listSeen = new Dictionary<string, bool[]>();
+
+            // gán list match sinh viên ban đầu là rỗng
+            for (int i=0; i<cCo; i++)
+            {
+                // khởi tạo danh sách sinh viên rỗng
+                List<Student> listSt = new List<Student>();
+
+                listMatched.Add(listCompany[i].Code, listSt);
+
+            }
+
+            // khởi tạo danh sách sinh vên đã nộp đơn là chưa nộp vào công ty nào
+            for (int j=0; j<cSt; j++)
+            {
+                bool[] seen = new bool[listStudent[j].listWish.Length];
+                listSeen.Add(listStudent[j].Code, seen);
+            }
+
+            // biến check xem đã duyệt hết nguyện vọng của sinh viên chưa
+            bool check = false;
+
+            // thực hiện match sinh viên
+            do
+            {
+                // tìm sinh viên chưa được ghép
+                Student st = new Student();
+                for (int i = 0; i < listStudent.Count; i++)
+                {
+                    // nếu sinh viên chưa match vào công ty nào và chưa duyệt hết danh sách nguyện vọng
+                    if (listStudent[i].CompanyCodeMatched == null && !listStudent[i].ThroughAll)
+                    {
+                        st = listStudent[i];
+                        break;
+                    }
+                }
+
+                // nếu như không tìm được sinh viên nào free nữa
+                if (st.Code == null)
+                {
+                    break;
+                }
+
+                // nếu sinh viên có danh sách nguyện vọng
+                if (st.listWish.Length == 0)
+                {
+                    st.ThroughAll = true;
+                    continue;
+                }
+
+                Company cp = new Company();
+                // lấy ra công ty nằm trong nguyện vọng của sinh viên và sinh viên chưa nộp đơn
+                for (int i=0; i< listSeen[st.Code].Length; i++)
+                {
+                    // nếu đã trải qua hết các công ty
+                    if (i == listSeen[st.Code].Length - 1)
+                    {
+                        st.ThroughAll = true;
+                    }
+
+                    if (!listSeen[st.Code][i])
+                    {
+                       foreach(var item in listCompany)
+                        {
+                            if (item.Code.ToLower() == (st.listWish[i].Trim()).ToLower())
+                            {
+                                cp = item;
+                            }
+                        }
+
+                       // đánh dấu là đã nộp đơn
+                        listSeen[st.Code][i] = true;
+                        break;
+                    }
+                }
+
+                // nếu công ty đó vẫn chưa tuyển đủ người
+                if (listMatched[cp.Code].Count < cp.Quantity)
+                {
+                    // thêm sinh viên vào danh sách tuyển dụng
+                    listMatched[cp.Code].Add(st);
+
+                    // match công ty với sinh viên
+                    st.CompanyCodeMatched = cp.Code;
+                }
+                else
+                {
+                    // thêm sinh viên này vào danh sách để so sánh
+                    listMatched[cp.Code].Add(st);
+                    // match công ty với sinh viên
+                    st.CompanyCodeMatched = cp.Code;
+
+                    // sắp xếp danh sách sinh viên
+                    listMatched[cp.Code].Sort(delegate (Student x, Student y) {
+                        return y.GPA.CompareTo(x.GPA);
+                    });
+
+                    // lấy ra sinh viên cuối cùng
+                    Student lastStudent = listMatched[cp.Code][listMatched[cp.Code].Count - 1];
+                    // xóa sinh viên đó khỏi danh sách
+                    listMatched[cp.Code].RemoveAt(listMatched[cp.Code].Count - 1);
+
+                    // bỏ match của sinh viên đó ra để đi match với công ty khác
+                    lastStudent.CompanyCodeMatched = null;
+                }
+
+                check = true;
+
+                // kiểm tra sinh viên đã thăm hết nguyện vọng của mình chưa
+                foreach(var item in listStudent)
+                {
+                    // nếu có sinh viên vẫn chưa duyệt hết
+                    if(!item.ThroughAll)
+                    {
+                        check = false;
+                    }
+                }
+
+            }
+            while (!check);
+
+            // in kết quả ra bảng
+            Cursor.Current = Cursors.WaitCursor;
+            // danh sách kết quả matching
+            DataTable dt = new DataTable();
+
+            dt.Columns.Add("MSSV");
+            dt.Columns.Add("Họ và tên");
+            dt.Columns.Add("GPA");
+            dt.Columns.Add("Công ty thực tập");
+            for (int i=0; i<listStudent.Count; i++)
+            {
+                dt.Rows.Add();
+                dt.Rows[dt.Rows.Count - 1][0] = listStudent[i].Code;
+                dt.Rows[dt.Rows.Count - 1][1] = listStudent[i].Name; 
+                dt.Rows[dt.Rows.Count - 1][2] = listStudent[i].GPA;
+                dt.Rows[dt.Rows.Count - 1][3] = listStudent[i].CompanyCodeMatched;
+            }
+
+            dataGrid.DataSource = dt.DefaultView;
+            Cursor.Current = Cursors.Default;
+
+            // danh sách kết quả tuyển dụng của các công ty
+            DataTable dtCom = new DataTable();
+            dtCom.Columns.Add("Mã Công ty");
+            dtCom.Columns.Add("Số Lượng");
+            for (int i = 0; i < listMatched.Count; i++)
+            {
+                dtCom.Rows.Add();
+                dtCom.Rows[dtCom.Rows.Count - 1][0] = listCompany[i].Code;
+                dtCom.Rows[dtCom.Rows.Count - 1][1] = listMatched[listCompany[i].Code].Count;
+            }
+
+            // xuất file excel
+            try
+            {
+                using (XLWorkbook workbook = new XLWorkbook())
+                {
+                    workbook.Worksheets.Add(dt, "Danh sách thực tập");
+                    workbook.Worksheets.Add(dtCom, "Kết quả tuyển dụng");
+                    workbook.SaveAs("Danh sách thực tập.xlsx");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi xuất file");
+            }
+        }
+    }
+
+    // model sinh viên
+    class Student
+    {
+        // mã số sinh viên
+        public string Code { set; get; }
+        // tên sinh viên
+        public string Name { set; get; }
+        // gpa
+        public double GPA { set; get; }
+        // danh sách công ty mong muốn vào thực tập, xếp theo thứ tự ưu tiên giảm dần.
+        public string[] listWish { set; get; }
+        // mã công ty đã ghép
+        public string CompanyCodeMatched { set; get; }
+        // đã nộp hết nguyện vọng vào công ty chưa
+        public bool ThroughAll { set; get; }
+    }
+
+    // model công ty
+    class Company
+    {
+        // mã công ty
+        public string Code { set; get; }
+        // tên công ty
+        public string Name { set; get; }
+        // số lượng tuyển dụng
+        public double Quantity { set; get; }
+    }
+}
